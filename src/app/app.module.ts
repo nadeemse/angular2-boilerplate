4a1c74314bfcc9eb6962b3cc84cf4921e6794bb9
nadeemswich <nadeem@myswich.com>
2016-09-20 12:32:25 +0400

    Adding Dist folder for temp use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routing } from './app.routing';
 
@@ -28,7 +29,7 @@ import { FooterComponent } from './layout/footer/footer.component';
     AlertModule,
     routing
   ],
-  providers: [],
+ providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
