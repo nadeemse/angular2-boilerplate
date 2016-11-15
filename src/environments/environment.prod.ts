771caececa0053a97a9c043f31293c34bd9e5507
Nadeem <nadeemakhtar.se@gmail.com>
2016-11-16 02:26:48 +0400

    rename public folder to assets as per upgraded angular cli version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://YOUR_LIVE_SERVER/api/',
+  API_TOKEN: 'YOU_AUTH_TOKEN'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://YOURLOCAL_SERVER/api/',
+  API_TOKEN: 'YOU_AUTH_TOKEN'
 };
